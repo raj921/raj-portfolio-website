119dcf3bf08ffa52d726906b4aa48fcf52173e2b
raj921 <79806602+raj921@users.noreply.github.com>
2024-06-04 16:55:59 +0530

    update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,36 +1,24 @@
 import { BrowserRouter } from "react-router-dom";
 import { Container } from "./styles";
-import ScrollAnimation from "react-animate-on-scroll";
+import { Fade } from "react-awesome-reveal";
 import Illustration from "../../assets/illustration.svg";
 import { NavHashLink } from "react-router-hash-link";
 import linkedin from '../../assets/linkedin.svg';
 import githubIcon from '../../assets/github.svg';
-
 import Hello from '../../assets/Hello.gif';
-
 
 export function Hero() {
   return (
     <Container id="home">
       <div className="hero-text">
-        <ScrollAnimation animateIn="fadeInUp">
+        <Fade cascade damping={0.2}>
           <p>Hello <img src={Hello} alt="Hello" width="20px"/>, I'm</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
           <h1>Guthala Rajkumar</h1>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
           <h3>Full Stack Developer</h3>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
           <p className="small-resume">1 Year of Experience</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
           <BrowserRouter>
             <NavHashLink smooth to="#contact" className="button">Contact</NavHashLink>
           </BrowserRouter>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
           <div className="social-media">
             <a
               href="https://www.linkedin.com/in/raj-kumar-4ab595209/"
@@ -52,12 +40,12 @@ export function Hero() {
               rel="noreferrer"
             ></a>
           </div>
-        </ScrollAnimation>
+        </Fade>
       </div>
       <div className="hero-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={1 * 1000}>
+        <Fade direction="right" delay={1000}>
           <img src={Illustration} alt="Illustration" />
-        </ScrollAnimation>
+        </Fade>
       </div>
     </Container>
   );
